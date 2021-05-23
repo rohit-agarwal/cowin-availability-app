4f73cf457d9310a806d3c0777d3f9407a345bf31
Rohit Agarwal <rohitrajagarwal@gmail.com>
2021-05-23 20:14:53 +0530

    adding instructions to mainactivity.kt

--- app/src/main/java/com/ragarwal/cowinavailabilityapp/MainActivity.kt
+++ app/src/main/java/com/ragarwal/cowinavailabilityapp/MainActivity.kt
@@ -10,9 +10,12 @@ class MainActivity : AppCompatActivity() {
 
         val phone_number = R.id.phone_number_text
 
-        if(phone_number.toString() == ""){
-            //do something here. some more comment text.
-            //adding more comments
+        if(!(phone_number.toString() == "" || phone_number.toString()=="Enter Phone number here")){
+            //phone number found.
+            //check if phone number is valid
+            //check if phone number delivers OTP to app. if not, display message to user that OTP not found.
+            //store phonenumber to a file.
         }
+
     }
 }
